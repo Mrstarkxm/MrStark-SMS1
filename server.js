5488946c4eede95bb5cd98cbad6107e9f2f59506
Mrstarkxm <zanintojistark@gmail.com>
2026-09-17 23:13:41 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ api['GET /api/health'] = async (req, res) => {
   const persistence = db.getPersistenceStatus();
   sendJson(res, 200, {
     ok: true,
-    build: 'V80',
+    build: 'V81',
     vercel: Boolean(process.env.VERCEL),
     persistence
   });
